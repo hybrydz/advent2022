92dc3d326bd8e9720827555d3c13515f5b70bc34
Pawan Malhotra <pawan.malhotra@Pawans-MacBook-Pro.local>
2023-11-10 19:01:56 +1100

    Day4 Part2

--- day4/day4.py
+++ day4/day4.py
@@ -5,20 +5,27 @@
 with open('input_day4.txt', "r") as file:
     elfPair = file.read().rstrip().splitlines()
 
-count = 0
-
+count = 0  #Part 1
+overlap = 0 #Part 2
 for pair in elfPair:
     firstPair = pair.split(',')[0].split('-')
     secondPair = pair.split(',')[1].split('-')
     #  Test Cases
     if (firstPair == secondPair):
         count += 1
-        print(firstPair,secondPair)
+        overlap += 1 
     elif (int(firstPair[0])<= int(secondPair[0])) and (int(firstPair[1])>= int(secondPair[1])):
-        # print(firstPair,secondPair)
         count += 1
+        overlap += 1 
     elif (int(secondPair[0])<= int(firstPair[0])) and (int(secondPair[1])>= int(firstPair[1])) :
         count += 1
-        # print(firstPair,secondPair)
+        overlap += 1   
+    # Part2
+    else:
+        if int(secondPair[0]) <= int(firstPair[0]) <= int(secondPair[1]):
+            overlap += 1
+        elif int(secondPair[0]) <= int(firstPair[1]) <= int(secondPair[1]):
+            overlap += 1
 
 print(count)
+print(overlap)
