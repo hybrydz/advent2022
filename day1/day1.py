c664f21fd21e519a3db373adc8f96f5beab36fe4
Pawan Malhotra <pawan.malhotra@Pawans-MacBook-Pro.local>
2023-11-10 15:34:32 +1100

    Day1

--- day1/day1.py
+++ day1/day1.py
@@ -4,18 +4,6 @@
 with open('input_day1.txt', "r") as file:
     elfInventory = file.read().rstrip().split('\n\n')
 
-# #  Part 1
-# totalCalories = 0
-# for invetory in elfInventory:
-#     sum = 0
-#     for item in invetory.split():
-#         sum += int(item)
-#     if sum > totalCalories:
-#         totalCalories = sum
-
-# print(totalCalories)
-
-# Part 2 
 totalCalories = []
 for invetory in elfInventory:
     sumCal = 0
@@ -23,5 +11,10 @@ for invetory in elfInventory:
         sumCal += int(item)
     totalCalories.append(sumCal)
 totalCalories.sort(reverse=True)
+
+## Part 1
+print(totalCalories[0])
+
+## Part 2
 print(sum(totalCalories[0:3]))
 
