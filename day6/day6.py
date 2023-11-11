052be4bcfd4dbef0904527944c26381e614af353
Pawan Malhotra <pawan.malhotra@Pawans-MacBook-Pro.local>
2023-11-11 17:19:08 +1100

    Day6 Part2

--- day6/day6.py
+++ day6/day6.py
@@ -1,10 +1,10 @@
-with open('test_input.txt', "r") as file:
-    lines = file.read().splitlines()
-
-# with open('input_day6.txt', "r") as file:
+# with open('test_input.txt', "r") as file:
 #     lines = file.read().splitlines()
 
-sum =0
+with open('input_day6.txt', "r") as file:
+    lines = file.read().splitlines()
+
+# Part 1
 for line in lines:
     startOfPacket = 0
     window = 4
@@ -18,7 +18,25 @@ for line in lines:
             index = startOfPacket
         else:
             if index == window:
-                sum+=window
+                print(window," ",line[startOfPacket:window])
+                break
+            else:
+                index+=1
+
+# Part 2
+for line in lines:
+    startOfPacket = 0
+    window = 14
+    index =0
+    while window<=len(line):
+        # print(index,startOfPacket,window)
+        # print(line[index],line[startOfPacket:window], line[startOfPacket:window].count(line[index]))
+        if line[startOfPacket:window].count(line[index])>1:
+            window +=1
+            startOfPacket+=1
+            index = startOfPacket
+        else:
+            if index == window:
                 print(window," ",line[startOfPacket:window])
                 break
             else:
